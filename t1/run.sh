34130d9efcdc8b12e935f743ebd35b225c41da95
Guilherme Taschetto <gtaschetto@gmail.com>
2015-05-01 17:09:03 -0300

    Holy little potato, Batman! Forgot to add the renamed dir. :P

---
+++ t1/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+timestamp=$(date +"%s")
+simulator="./bin/simulator.jar"
+simulations_path="./simulations/"
+model_path="$1"
+model_name=$(basename $1)
+model_name=${model_name%.*}
+output="${simulations_path}${model_name}_${timestamp}.txt"
+
+
+simulate() {
+    echo "Simulating model: $model_path"
+    echo "          Output: ${output}"
+    java -jar $simulator run $model_path >> ${output}
+}
+
+simulate
